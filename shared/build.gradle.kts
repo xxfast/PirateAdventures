b073e91b61d52ec6af2f414b45bfd7dccc2f52de
xxfast <isurukusumal36@gmail.com>
2023-09-28 15:22:44 +1000

    Add shared module

---
+++ shared/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.compose")
+    id("kotlin-parcelize")
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
+
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.getMajorVersion()
+            }
+        }
+    }
+
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                implementation(compose.materialIconsExtended)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+android {
+    namespace = "io.github.xxfast.pirate.adventures.shared"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 24
+    }
+}
---
+++ shared/src/commonMain/kotlin/io/github/xxfast/pirate/adventures/shared/screens/ship/ShipView.kt
@@ -0,0 +1,8 @@
+package io.github.xxfast.pirate.adventures.shared.screens.ship
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShipView() {
+
+}
